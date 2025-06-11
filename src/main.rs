f164cbf9762ab2e4fe21604655f166e090f5cb9a
Karn Wong <karn@karnwong.me>
2025-06-11 14:16:01 +0700

    add relay

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let builder = iroh::Endpoint::builder();
+    let builder = iroh::Endpoint::builder().relay_mode(iroh::RelayMode::Default);
     let endpoint = builder.bind().await?;
     println!("node id: {:?}", endpoint.node_id());
 
